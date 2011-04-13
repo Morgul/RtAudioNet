bdc9ac71118246f28aee5a662a98b49ef7ec0500
Morgul_cp <SND\Morgul_cp>
2011-04-13 15:29:34 +0000

    Still learning TFS. Checking in rest of files.

---
+++ RtAudioNet Test Suite/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using RtAudioNet;
+
+namespace RtAudioNet_Test_Suite
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            RtAudio rtaudio = new RtAudio();
+            RtAudio.DeviceInfo info = new RtAudio.DeviceInfo();
+        }
+    }
+}
