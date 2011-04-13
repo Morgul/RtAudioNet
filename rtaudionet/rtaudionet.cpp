d112f0e4aacaece479a1c12de50413a3de3e668b
Christopher S. Case <chris.case@g33xnexus.com>
2011-04-13 22:01:29 +0000

    Finished wrapping RtAudio. Minimal testing done, but it seems to work!

--- rtaudionet/rtaudionet.cpp
+++ rtaudionet/rtaudionet.cpp
@@ -1,6 +1,11 @@
 // This is the main DLL file.
 
+#include <msclr\marshal.h>
+#include <msclr\marshal_cppstd.h>
+
 #include "rtaudionet.h"
+
+using namespace msclr::interop;
 
 namespace RtAudioNet
 {
@@ -9,8 +14,21 @@ namespace RtAudioNet
 	/// RtAudio Class
 
 	// A static function to determine the available compiled audio APIs.
-	void RtAudio::getCompiledApi(List<RtAudio::Api> ^apis)
+	List<RtAudio::Api>^ RtAudio::getCompiledApi()
 	{
+		List<RtAudio::Api>^ apiList = gcnew List<RtAudio::Api>();
+		::std::vector<::RtAudio::Api> _apiList;
+
+		// Call the unmanaged function
+		::RtAudio::getCompiledApi(_apiList);
+		
+		// Iterate over the vector and populate apiList
+		for(std::vector<::RtAudio::Api>::iterator it = _apiList.begin(); it != _apiList.end(); ++it) 
+		{
+			apiList->Add(static_cast<RtAudio::Api>(*it));
+		} // end for
+
+		return apiList;
 	} // end getCompiledApi
 		
 	// The class constructor.
@@ -30,97 +48,105 @@ namespace RtAudioNet
 	// The destructor.
 	RtAudio::~RtAudio()
 	{
+		delete _rtaudio;
 	} // end ~RtAudio
 	
 	// Returns the audio API specifier for the current instance of RtAudio.
 	RtAudio::Api RtAudio::getCurrentApi()
 	{
-		return Api::UNSPECIFIED;
+		return static_cast<RtAudio::Api>(_rtaudio->getCurrentApi());
 	} // end getCurrentApi
 	
 	// A public function that queries for the number of audio devices available.
 	unsigned int RtAudio::getDeviceCount()
 	{
-		return 0;
+		return _rtaudio->getDeviceCount();
 	} // end getDeviceCount
 	
 	// Return an RtAudio::DeviceInfo structure for a specified device number.
 	RtAudio::DeviceInfo^ RtAudio::getDeviceInfo(unsigned int device)
 	{
-		return gcnew DeviceInfo();
+		// Get the unmanaged DeviceInfo class
+		return gcnew RtAudio::DeviceInfo(_rtaudio->getDeviceInfo(device));
 	} // end getDeviceInfo
 	
 	// A function that returns the index of the default output device.
 	unsigned int RtAudio::getDefaultOutputDevice()
 	{
-		return 0;
+		return _rtaudio->getDefaultOutputDevice();
 	} // end getDefaultOutputDevice
 	
 	// A function that returns the index of the default input device.
 	unsigned int RtAudio::getDefaultInputDevice()
 	{
-		return 0;
+		return _rtaudio->getDefaultInputDevice();
 	} // end getDefaultInputDevice
 	
 	// A public function for opening a stream with the specified parameters.
-	void RtAudio::openStream( RtAudio::StreamParameters^ outputParameters, RtAudio::StreamParameters^ inputParameters, RtAudioFormat^ format, 
+	void RtAudio::openStream(RtAudio::StreamParameters^ outputParameters, RtAudio::StreamParameters^ inputParameters, RtAudioFormat format, 
 		unsigned int sampleRate, unsigned int *bufferFrames, RtAudioCallback callback, void *userData, RtAudio::StreamOptions^ options)
 	{
+		_rtaudio->openStream(convertManagedToUnmanaged(outputParameters), convertManagedToUnmanaged(inputParameters), format, sampleRate, bufferFrames, callback, userData, convertManagedToUnmanaged(options));
 	} // end openStream
 	
 	// A function that closes a stream and frees any associated stream memory.
 	void RtAudio::closeStream()
 	{
+		_rtaudio->closeStream();
 	} // end closeStream()
 	
 	// A function that starts a stream.
 	void RtAudio::startStream()
 	{
+		_rtaudio->startStream();
 	} // end startStream
 	
 	// Stop a stream, allowing any samples remaining in the output queue to be played.
 	void RtAudio::stopStream()
 	{
+		_rtaudio->stopStream();
 	} // end stopStream
 	
 	// Stop a stream, discarding any samples remaining in the input/output queue.
 	void RtAudio::abortStream()
 	{
+		_rtaudio->abortStream();
 	} // end abortStream
 	
 	// Returns true if a stream is open and false if not.
 	bool RtAudio::isStreamOpen()
 	{
-		return false;
+		return _rtaudio->isStreamOpen();
 	} // end isStreamOpen()
 	
 	// Returns true if the stream is running and false if it is stopped or not open.
 	bool RtAudio::isStreamRunning()
 	{
-		return false;
+		return _rtaudio->isStreamRunning();
 	} // end isStreamRunning
 	
 	// Returns the number of elapsed seconds since the stream was started.
 	double RtAudio::getStreamTime()
 	{
-		return 0.0;
+		return _rtaudio->getStreamTime();
 	} // end getStreamTime
 	
 	// Returns the internal stream latency in sample frames.
 	long RtAudio::getStreamLatency()
 	{
-		return 0;
+		return _rtaudio->getStreamLatency();
 	} // end getStreamLatency
 	
 	// Returns actual sample rate in use by the stream.
 	unsigned int RtAudio::getStreamSampleRate()
 	{
-		return 0;
+		return _rtaudio->getStreamSampleRate();
 	} // end getStreamSampleRate
 	
 	// Specify whether warning messages should be printed to stderr.
 	void RtAudio::showWarnings(bool value)
 	{
+		_rtaudio->showWarnings(value);
 	} // end showWarnings
 	
 	/// Protected \\\
@@ -128,9 +154,36 @@ namespace RtAudioNet
 	// Actual constructor. This is a work around for issues using default parameters in managed C++.
 	void RtAudio::initialize(RtAudio::Api api)
 	{
+		_rtaudio = new ::RtAudio(::RtAudio::Api(int(api)));
 	} // end initialize
 
-	void RtAudio::openRtApi(RtAudio::Api api)
+	// Converstion of StreamParameters^ to StreamParameters*
+	::RtAudio::StreamParameters* RtAudio::convertManagedToUnmanaged(RtAudio::StreamParameters^ _params)
 	{
-	} // end openRtApit
+		::RtAudio::StreamParameters* params = new ::RtAudio::StreamParameters();
+
+		params->deviceId = _params->deviceId;
+		params->nChannels = _params->nChannels;
+		params->firstChannel = _params->firstChannel;
+		
+		return params;
+	} // end convertManagedToUnmanaged
+
+	// Converstion of StreamOptions^ to StreamOptions*
+	::RtAudio::StreamOptions* RtAudio::convertManagedToUnmanaged(RtAudio::StreamOptions^ _options)
+	{
+		::RtAudio::StreamOptions* options = new ::RtAudio::StreamOptions();
+
+		options->numberOfBuffers = _options->numberOfBuffers;
+
+		// Convert streamName
+		String^ name = gcnew String(_options->streamName);
+		std::string streamName(marshal_as<std::string>(name));
+		options->streamName = streamName;
+		
+		options->priority = _options->priority;
+
+		return options;
+	} // end convertManagedToUnmanaged
+
 } // end namespace
