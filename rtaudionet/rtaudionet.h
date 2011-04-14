38d28090683585e5e295e61c6522441010e0862a
Morgul_cp <SND\Morgul_cp>
2011-04-14 15:32:37 +0000

    Added CallbackInfo struct to wrapper.

--- rtaudionet/rtaudionet.h
+++ rtaudionet/rtaudionet.h
@@ -213,6 +213,21 @@ namespace RtAudioNet {
 
 	};
 
+	// This global structure type is used to pass callback information
+	// between the private RtAudio stream structure and global callback
+	// handling functions.
+	ref struct CallbackInfo {
+		void *object;			// Used as a "this" pointer.
+		ThreadHandle thread;
+		void *callback;
+		void *userData;
+		void *apiInfo;			// void pointer for API specific callback information
+		bool isRunning;
+		
+		// Default constructor.
+		CallbackInfo() : object(0), callback(0), userData(0), apiInfo(0), isRunning(false) {}
+	};
+
 	public ref class RtApi
 	{
 	};
